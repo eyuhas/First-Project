859cdc3db33346c856b4835bf45617299bfcfc3f
eyuhas <elisabethyuhas@me.com>
2016-10-13 15:06:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 function setup() {
   createCanvas(3000, 3000);
+  background( 20, 50, 60);
 }
 
 function draw() {
 ellipse(70, 70, 70, 70);
+  fill( 155, 0, 0);
 ellipse(mouseX, mouseY, 50, 70);
+  stroke(0, 0, 200
 }
