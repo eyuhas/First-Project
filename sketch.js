11ae266dcd3efdf9babfeb7c64887b4d97c86b19
eyuhas <elisabethyuhas@me.com>
2016-10-14 09:23:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
+
 function setup() {
   createCanvas(3000, 3000);
   background( 120, 0, 120, 120);
 }
 
 function draw() {
-  if (keyIsPressed == true){
+  if (keyCode == LEFT_ARROW) {
+    fill(0,0, 0);
+    else if (keyCode == RIGHT_ARROW){
+      fill(255, 255, 255);
+  if (keyIsPressed){
     fill(255, 0, 0);
   }
   else{
