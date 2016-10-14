5971fd06c87a7898fa114119c29793aa9fc72e47
eyuhas <elisabethyuhas@me.com>
2016-10-14 08:45:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup() {
   createCanvas(3000, 3000);
-  background( 255, 255, 255);
+  background( 255, 0, 255);
 }
 
 function draw() {
   fill( 0, 102, 255, 60);
-triangle(mouseX + 10, mouseY, mouseX - 10, mouseY, mouseX ,mouseY + 10);
+triangle(mouseX + 10, mouseY, mouseX - 10, mouseY, mouseX ,mouseY - 10);
 }
